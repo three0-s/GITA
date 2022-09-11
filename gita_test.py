cfa30ff02d8d33725236205d01d46ee8c3fcf194
Yewon Lim <ga06033@yonsei.ac.kr>
2022-09-11 18:59:18 +0000

    220912 | super_res_GITA

--- gita_test.py
+++ gita_test.py
@@ -1,4 +1,4 @@
-from gita.utils.model_creation import create_model, model_and_diffusion_defaults
+from gita.utils.model_creation import create_model, model_and_diffusion_defaults, model_and_diffusion_defaults_upsampler
 import clip
 import torch
 import numpy as np
@@ -8,11 +8,12 @@ img_encoder = clip_model.visual
 
 condi = torch.zeros(1, 3, 224, 224)
 timestep=torch.tensor([100])
-dummy = torch.zeros(1, 3, 64, 64)
-model_kwargs = model_and_diffusion_defaults()
+dummy = torch.zeros(1, 3, 256, 256)
+model_kwargs = model_and_diffusion_defaults_upsampler()
 model_kwargs.update(dict(num_channels=128, num_res_blocks=3))
 print(img_encoder.output_dim)
 model = create_model(img_encoder=img_encoder, encoding_dim=img_encoder.output_dim, aug_level=0.1, **model_kwargs)
 print("Model parameters:", f"{np.sum([int(np.prod(p.shape)) for p in model.parameters() if p.requires_grad ]):,}")
 out = model(dummy, timestep, condi)
 print(out.shape)
+print(type(model))
